8edf5fa76a39b16929601de2c789753b79e160a8
open_mailbox <dev@open-mailbox.com>
2020-11-16 23:49:10 -0500

    Implement proof of work.

--- lib/rbcoin/blockchain.rb
+++ lib/rbcoin/blockchain.rb
@@ -4,33 +4,34 @@ module RbCoin
       Digest::SHA256.hexdigest(block.sort.to_h.to_json)
     end
 
+    def self.valid_block(block)
+      block[:hash][0,4] == "0000"
+    end
+
     def initialize
       @chain                = []
       @pending_transactions = []
 
       puts "Creating genesis block."
-
-      new_block
+      @chain.push(new_block)
     end
 
     def last_block
       @chain[-1]
     end
 
-    def new_block(previous_hash = nil)
+    def new_block
       block = {
         index:         @chain.length,
-        previous_hash: previous_hash,
+        nonce:         rand(10 ** 20).to_s(16),
+        previous_hash: last_block && last_block[:hash],
         timestamp:     Time.now.utc,
         transactions:  @pending_transactions
       }
 
       block[:hash] = Blockchain.hash(block)
-
       @pending_transactions = []
-      @chain.push(block)
 
-      puts "Created block #{block[:index]}"
       block
     end
 
@@ -40,6 +41,19 @@ module RbCoin
         recipient: recipient,
         amount:    amount
       })
+    end
+
+    def proof_of_work
+      block = nil
+
+      while true do
+        block = new_block
+        break if Blockchain.valid_block(block)
+      end
+
+      @chain.push(block)
+
+      puts "Found a new block: #{block}"
     end
   end
 end
